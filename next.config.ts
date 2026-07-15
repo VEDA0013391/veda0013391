474b531d1c3ee465e629a2db98ee091c184cf69b
VEDA0013391 <sayamina.56@gmail.com>
2026-07-15 12:30:58 +0900

    basepath

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithub = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isGithub ? "/veda0013391" : "",
+  assetPrefix: isGithub ? "/veda0013391/" : "",
 };
 
 export default nextConfig;
